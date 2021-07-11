7bd5f288119386ebaeb5b48ea17eb322343f043a
Melania Tusa <letitia.tusa@yahoo.com>
2021-07-11 21:35:28 +0300

    This is my first commit for lab_10

---
+++ src/ro/fasttrackit/mela/teme/OperationArithmeticCalculator.java
@@ -0,0 +1,49 @@
+/**
+ * Autor : Tusa Letitia-Melania
+ * Implement an arithmetic calculator that takes 3 arguments: the first number,
+ * the operation, the second number
+ * The arguments entered are assumed to be valid
+ * The validation part of the arguments was not done here
+ */
+package ro.fasttrackit.mela.teme;
+
+public class OperationArithmeticCalculator {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Numar insuficient de argumente : " + args.length);
+            return;
+        }
+        if (args.length == 3) {
+            double a = Double.parseDouble(args[0]);
+            double b = Double.parseDouble(args[2]);
+            String operator = args[1];
+            if (operator.equals("+")) {
+                System.out.println("The chosen operator is Addition:");
+                System.out.print(a + " " + operator + " " + b + " = " + (a + b));
+                return;
+            }
+            if (operator.equals("-")) {
+                System.out.println("The chosen operator is Subtraction:");
+                System.out.print(a + " " + operator + " " + b + " = " + (a - b));
+                return;
+            }
+            if (operator.equals("*")) {
+                System.out.println("The chosen operator is Multiplication:");
+                System.out.print(a + " " + operator + " " + b + " = " + (a * b));
+                return;
+            }
+            if (operator.equals("/")) {
+                System.out.println("The chosen operator is Dividen:");
+                System.out.print(a + " " + operator + " " + b + " = " + (a / b));
+                return;
+            }
+            if (operator.equals("%")) {
+                System.out.println("The chosen operator is Remainder:");
+                System.out.print(a + " " + operator + " " + b + " = " + (a % b));
+                return;
+            }
+            // implement calculator logic here
+            // System.out.println("Resultis: " + result);
+        }
+    }
+}
