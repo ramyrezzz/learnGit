546c2beb1fd8492cc089d1c5541157c7929a9999
Rami Sharaiyri <ramy_ab@yahoo.com>
2021-07-12 11:07:00 +0300

    Fixed the project structure, moved assignment to proper package

---
+++ src/ro/fasttrackit/assignments/tomi/Lab10Assignment.java
@@ -0,0 +1,93 @@
+package ro.fasttrackit.assignments.tomi;
+
+import java.util.Scanner;
+
+public class Lab10Assignment {
+
+    public static void main(String[] args) {
+
+        System.out.println("Year " + args[0]);
+        int theYear = Integer.parseInt(args[0]);
+        System.out.println(theYear);
+
+        if (theYear % 4 == 0 && theYear % 100 != 0 || theYear % 400 == 0) {
+            System.out.println("In the year " + theYear + " February has 29 days !");
+        } else if (theYear % 4 != 0 || theYear % 100 == 0 || theYear % 400 != 0) {
+            System.out.println("In the year " + theYear + " February has 28 days !");
+        }
+
+
+
+//        if(args.length == 3){
+//            double a = Double.parseDouble(args[0]);
+//            double b = Double.parseDouble(args[2]);
+//            String operator = args[1];
+//            if(operator.equals("+")){
+//                double result = a + b;
+//                System.out.println("Result is: " + result);
+//            }
+//            else if(operator.equals("-")){
+//                double result = a - b;
+//                System.out.println("Result is: " + result);
+//            }
+//            else if(operator.equals("*")){
+//                double result = a * b;
+//                System.out.println("Result is: " + result);
+//            }
+//            else if(operator.equals("/")) {
+//                double result = a / b;
+//                System.out.println("Result is: " + result);
+//            }
+//        }
+//        else {
+//            System.err.println("3 args are needed !");
+//        }
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter string for verify :");
+//        String typedString = sc.next();
+//        String reversedString ="";
+//        for (int j = typedString.length() - 1; j >= 0; j--) {
+//            reversedString = reversedString + typedString.charAt(j);
+//        }
+//        System.out.println(reversedString);
+//        if (typedString.equals(reversedString))
+//        {
+//            System.out.println("IS PALINDROME");
+//        }
+//        else
+//        {
+//            System.out.println("IS NOT PALINDROME");
+//        }
+
+
+//
+//        System.out.println("The number " + args[0]);
+//        int limit = Integer.parseInt(args[0]);
+//        int i =0;
+//        int num =0;
+//        String primeNumbers = " ";
+//
+//        System.out.println("Enter a number:");
+//        for (i = 1; i <= limit; i++)
+//        {
+//            int counter=0;
+//            for(num =i; num>=1; num--)
+//            {
+//                if(i%num==0)
+//                {
+//                    counter = counter + 1;
+//                }
+//            }
+//            if (counter ==2)
+//            {
+//                primeNumbers = primeNumbers + i + " ";
+//            }
+//        }
+//        System.out.println("Prime numbers between 1 and 100 are:");
+//        System.out.println(primeNumbers);
+    }
+}
+
