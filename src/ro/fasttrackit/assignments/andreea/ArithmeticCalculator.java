c13e1e04291d4250779098e2ff8e3cfaee2520a8
Rami Sharaiyri <ramy_ab@yahoo.com>
2021-07-12 19:06:47 +0300

    Refactred Arithmetic class for the main math operation

--- src/ro/fasttrackit/assignments/andreea/ArithmeticCalculator.java
+++ src/ro/fasttrackit/assignments/andreea/ArithmeticCalculator.java
@@ -3,31 +3,40 @@ package ro.fasttrackit.assignments.andreea;
 public class ArithmeticCalculator {
 
     public static void main(String[] args) {
-        if (args.length == 3) {
-            double a = Double.parseDouble(args[0]);
-            double b = Double.parseDouble(args[2]);
-            String[] operator = {"+", "-", "*", "/"};
-            double result = 0;
-            for (int i = 0; i < operator.length; i++) {
-                if (operator[i] == "+") {
-                    result = a + b;
-                    System.out.println("Result is: " + result);
-                }
-                if (operator[i] == "-") {
-                    result = a - b;
-                    System.out.println("Result is: " + result);
-                }
-                if (operator[i] == "*") {
-                    result = a * b;
-                    System.out.println("Result is: " + result);
-                }
-                if (operator[i] == "/") {
-                    result = a / b;
-                    System.out.println("Result is: " + result);
-                }
-            }
-        } else {
+        if (args.length != 3) {
             System.err.println("3 args are needed!");
+            return;
         }
+        double firstNr = Double.parseDouble(args[0]);
+        double secondNr = Double.parseDouble(args[2]);
+        String operation = args[1];
+        makeCalculations(firstNr, secondNr, operation);
+    }
+
+    private static void makeCalculations(double a, double b, String operator) {
+        if (isAddition(operator))
+            System.out.println("Result is: " + a + b);
+        if (isSubtraction(operator))
+            System.out.println("Result is: " + (a - b));
+        if (isMultiplication(operator))
+            System.out.println("Result is: " + a * b);
+        if (isDivision(operator))
+            System.out.println("Result is: " + a / b);
+    }
+
+    private static boolean isDivision(String operator) {
+        return operator.equals("/");
+    }
+
+    private static boolean isMultiplication(String operator) {
+        return operator.equals("*");
+    }
+
+    private static boolean isSubtraction(String operator) {
+        return operator.equals("-");
+    }
+
+    private static boolean isAddition(String operator) {
+        return operator.equals("+");
     }
 }
