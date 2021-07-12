ad9d96ce9370e909cdb3cf7f117b131fa38bdc2d
unknown <Sucala_adrian28@yahoo.com>
2021-07-12 20:43:37 +0300

    My homework

---
+++ temaLab9.java
@@ -0,0 +1,31 @@
+package ro.FasttrackIT;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPalindrome(int a) {
+        int original = a;
+        int reversed = 0;
+        while (a != 0) {
+            int remainder = a % 10;
+            reversed = reversed * 10 + remainder;
+            a = a / 10;
+        }
+        if(original == reversed)
+            return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+	    // Read the number n from keyboard
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = in.nextInt();
+        if (isPalindrome(n))
+            System.out.println("The number " + n + " is palindrome");
+        else
+            System.out.println("The number " + n + " is not palindrome");
+    }
+}
