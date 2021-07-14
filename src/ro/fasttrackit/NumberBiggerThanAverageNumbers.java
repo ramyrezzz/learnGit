d769e442bddd17f1552baffeed947384963df107
rsferle <rsferle@yahoo.com>
2021-07-14 15:05:51 +0300

    Get the numbers bigger the average number of the list

---
+++ src/ro/fasttrackit/NumberBiggerThanAverageNumbers.java
@@ -0,0 +1,28 @@
+package ro.fasttrackit;
+
+public class NumberBiggerThanAverageNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,5,6,7,9};
+        int sum = 0;
+        int average = averageNumberOfTheList(numbers, sum);
+        listNumbersBiggerThanAverageNumber(numbers, average);
+
+    }
+
+    private static int averageNumberOfTheList(int[] numbers, int sum) {
+        for (int value: numbers) {
+            sum += value;
+        }
+        int average = sum / numbers.length;
+        return average;
+    }
+
+    private static int listNumbersBiggerThanAverageNumber(int[] numbers, int average) {
+        for (int search: numbers) {
+            if (search > average)
+                System.out.println(search);
+        }
+        return average;
+
+    }
+}
