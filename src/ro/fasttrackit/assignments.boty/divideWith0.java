a67f314bfe2014584462a0f18e384fefe7ab9ca0
Csiki Botond <csikibotondi@gmail.com>
2021-07-14 11:11:37 +0300

    The input coming into your application is different than 0

---
+++ src/ro/fasttrackit/assignments.boty/divideWith0.java
@@ -0,0 +1,32 @@
+package ro.fasttrackit;
+
+public class divideWith0 {
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("not enough numbers to calculate.");
+            return;
+        }
+        if ((args[0] != "0") || (args[1] != "0") || (args[2] != "0")) {
+            System.out.println("Numbers need to be greater than 0.");
+            return;
+        }
+        System.out.println("1st arg:" + args[0]);
+        System.out.println("2st arg:" + args[1]);
+        System.out.println("3st arg:" + args[2]);
+
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int sum = a + b;
+        int prod = a * b;
+        int division = prod / c;
+
+        String nameOfCalculator = "Digit Calculator";
+
+        System.out.printf("%s: %nSum is: %d and prod is: %d%n", nameOfCalculator, sum, prod);
+        System.out.println(nameOfCalculator + "\n" + "Sum is: " + sum + " and prod is: " + prod);
+    }
+}
