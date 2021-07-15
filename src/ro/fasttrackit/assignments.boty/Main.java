f501c61fa4aaf547761eb4cf82cb1302d7d728b2
Csiki Botond <csikibotondi@gmail.com>
2021-07-15 18:15:31 +0300

    lab-11-boty Homework

---
+++ src/ro/fasttrackit/assignments.boty/Main.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.assignments.boty;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person(args[0]);
+        person.isFull();
+        person.eat();
+//        person.run();
+//        person.drink();
+    }
+
+}
