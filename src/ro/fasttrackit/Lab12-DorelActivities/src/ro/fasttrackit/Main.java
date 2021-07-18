a66767a497622dea82a49147b0621d106ab57572
oltean <oltean.cristian@gmail.com>
2021-07-18 17:19:50 +0300

    DorelActivities

---
+++ src/ro/fasttrackit/Lab12-DorelActivities/src/ro/fasttrackit/Main.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person("Dorel");
+        person.isFull();
+        person.eat();
+        person.run();
+        person.dance();
+        person.tired();
+    }
+}
---
+++ src/ro/fasttrackit/Lab12-DorelActivities/src/ro/fasttrackit/Person.java
@@ -0,0 +1,38 @@
+package ro.fasttrackit;
+
+public class Person {
+    private String name;
+    private boolean full = false;
+    private boolean fit = false;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    public Person() {
+        this.name = name;
+    }
+    public String name() {
+        return name;
+    }
+    public void isFull() {
+        System.out.printf("%s is full: %s%n", name, full);
+    }
+    public void eat() {
+        full = true;
+        System.out.printf("Person %s eats.%n", name);
+        isFull();
+    }
+    public void run() {
+        System.out.printf("Person %s exercises.%n", name);
+    }
+    public void dance() {
+        fit = false;
+        System.out.printf("Person %s is dancing to lose weight.%n", name);
+        isFull();
+    }
+    public void tired() {
+        full = true;
+        System.out.printf("Person %s is tired because is full. %n", name);
+        isFull();
+    }
+}
