18386518556ee0d17e9418ee0139777514e1c660
Valerian1985 <valerianbuzila1985@gmail.com>
2021-07-19 18:47:52 +0300

    fixed homework

--- src/ro/fasttrackit/old/Homework.java
+++ src/ro/fasttrackit/old/Homework.java
@@ -1,31 +1,30 @@
 package ro.fasttrackit.old;
 
 public class Homework {
-    public class Main {
     public static void main(String[] args) {
         int sum = 1;
         int ctr = 0;
         int n = 0;
 
         while (ctr < 100) {
-        n++;
+            n++;
             if (n % 2 != 0) {
-            if (is_Prime(n)) {
-                sum += n;
+                if (is_Prime(n)) {
+                    sum += n;
+                }
             }
+            ctr++;
         }
-        ctr++;
+        System.out.println("\nSum of the prime numbers till 100: " + sum);
     }
-    System.out.println("\nSum of the prime numbers till 100: "+sum);
-}
 
     public static boolean is_Prime(int n) {
-        for (int i = 3; i * i <= n; i+= 2) {
+        for (int i = 3; i * i <= n; i += 2) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
-
+}
 
