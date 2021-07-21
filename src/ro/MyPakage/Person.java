2e103e016fb8e15760669956194e3ac59c6889bc
Valerian1985 <valerianbuzila1985@gmail.com>
2021-07-21 18:17:31 +0300

    Homework13

---
+++ src/ro/MyPakage/Person.java
@@ -0,0 +1,17 @@
+package ro.MyPakage;
+
+public class Person {
+    private final String name;
+    private boolean full = false;
+    public Person(String name){
+        this.name = name;
+    }
+    public String name(){return name;}
+    public void isFull(){
+        System.out.printf("%s is full: %s%n",name, full);
+    }
+    public void eat(){
+        System.out.printf("Person %s eats.%n", name);
+        isFull();
+    }
+}
