6a0808568cdfce6e513d4f2447219665e42c79df
Valerian1985 <valerianbuzila1985@gmail.com>
2021-07-21 18:17:31 +0300

    Homework13

---
+++ src/ro/MyPakage/Feedable.java
@@ -0,0 +1,13 @@
+package ro.MyPakage;
+
+public interface Feedable {
+    void eat();
+    void eat(String food);
+    void eat(String food, boolean isFull);
+
+    void drink();
+    void drink(String liquid);
+
+    void run();
+
+}
---
+++ src/ro/MyPakage/Main.java
@@ -0,0 +1,8 @@
+package ro.MyPakage;
+
+public class Main {
+    public static void main(String[] args){
+        PersonWithInterface firstPerson = new PersonWithInterface( "Valerian");
+        firstPerson.eat("Salad",false);
+    }
+}
