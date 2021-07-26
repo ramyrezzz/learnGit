2c930ea6a4e422c3330e9c45effe0104a259aefc
Mirela Groza <sasmirela2@gmail.com>
2021-07-26 15:54:28 +0300

    Lab11 Assignment

---
+++ src/ro/fasttrackitMirelaAssignments/NumberGreaterThanTheAverage.java
@@ -0,0 +1,35 @@
+package ro.fasttrackitMirelaAssignments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGreaterThanTheAverage {
+    public static void main(String[] args) {
+        int[] numbers = {4, 7, 3, 9, 5, 8};
+        int total = 0;
+        for(int number : numbers){
+            total += number;
+        }
+
+        int average = total / numbers.length;
+
+        System.out.println("The average of the given numbers is: " + average);
+        System.out.print("The number bigger than the average are: " );
+
+    for (int number : numbers){
+        if (number > average ){
+            System.out.print(number + " ");
+        }
+    }
+
+        }
+}
+
+
+
+
+
+
+
+
+
