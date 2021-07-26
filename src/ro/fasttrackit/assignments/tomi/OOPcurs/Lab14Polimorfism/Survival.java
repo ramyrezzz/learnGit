cb6c705e273c359ec3984093a881449436f90213
Simo Tamas <simo_tamas2007@yahoo.com>
2021-07-26 18:20:54 +0300

    sters ceva

--- src/ro/fasttrackit/assignments/tomi/OOPcurs/Lab14Polimorfism/Survival.java
+++
@@ -1,7 +0,0 @@
-package ro.fasttrackit.assignments.tomi.OOPcurs.Lab14Polimorfism;
-
-public abstract class Survival {
-    public void eat(){
-        System.out.println("This object eats ! ");
-    }
-}
